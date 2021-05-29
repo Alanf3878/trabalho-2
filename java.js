470875e8116d8f801f73b194fca8951c76b20df6
Alan Fernandes <alanf3878@outlook.com>
2021-05-29 06:57:11 -0300

    trabalho-2

---
+++ java.js
@@ -0,0 +1,46 @@
+let idade = 18
+if(idade >= 18 ){
+    console.log("voce é maior de idade")
+
+} else{
+    console.log("você não é maior de idade")
+
+}
+
+console.log(idade)
+
+let mês = "Dezembro"
+
+if ((mês === "Dezembro") || (mês === "Janeiro")){
+    console.log("Você faz aniversário em Dezembro ou Janeiro")
+} else{
+    console.log("Você não faz anirversário nesses meses")
+}
+
+
+
+let nome = "marx"
+
+if(nome > 6)
+console.log("Seu nome tem mais de 6 letras")
+
+else{
+    console.log("Seu nome nao tem nem 6 letras!")
+}
+
+let nombre = "Marx"
+
+if(nombre.substring((0,1)) === ("L")){
+    console.log("Caiu no IF")
+
+} else{
+    console.log ("Caiu no else")
+} 
+
+
+
+
+
+
+
+
