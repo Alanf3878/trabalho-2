03072180e13aedafe5ad2cfacda3b24b5543891f
Alan Fernandes <alanff3878@outlook.com>
2021-06-02 14:36:37 -0300

    arrumando

---
+++ index.js
@@ -0,0 +1,58 @@
+let idade = 19
+if(idade > 18 ){
+    console.log("voce é maior de 18")
+
+} else{
+    console.log("você não é maior de idade")
+
+}
+
+console.log(idade)
+
+let maturidade = 18
+let humana = true
+
+if (maturidade >= 18 && humana === true){
+    console.log ("Você é maior de idade e também é humano")
+} else{
+    console.log ("Você não é maior de idade e não é humano")
+}
+
+
+
+
+
+
+
+
+let mês = "Dezembro"
+
+if ((mês === "Dezembro") || (mês === "Janeiro")){
+    console.log("Você faz aniversário em Dezembro ou Janeiro")
+} else{
+    console.log("Você não faz anirversário nesses meses")
+}
+
+
+
+let nome = "romario"
+
+if(nome.substring((0,1)) || ("r")){
+console.log("seu nome começa com a letra r")
+}
+else{
+    console.log("Seu nome não começa com a letra r!")
+}
+
+
+let Nome = "ALan"
+let sobreName = "Fernandes"
+
+
+if(Nome.substring(0,1) === ("L") || (sobreName.length >6) ){
+    console.log("Seu sobrenome tem mais de 6 letras ou começa com a letra L  ")
+} else{
+    console.log("Seu nome NÃO comeca com a letra L ou não tem mais de 6 letras")
+}
+
+
